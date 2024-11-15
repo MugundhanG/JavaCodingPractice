5b9f36760a4408893b43b0f6122f14427a4339b6
Mugundhan G <mugundhan10@gmail.com>
2024-11-15 08:03:54 +0530

    Learned how to override thread run method and start the thread and also used thread.sleep() method to pause a thread for particular time

--- src/Concurrency/Threads/ThreadMain.java
+++ src/Concurrency/Threads/ThreadMain.java
@@ -2,6 +2,7 @@ package Concurrency.Threads;
 
 public class ThreadMain {
     public static void main(String[] args) {
-
+        ThreadPractice t = new ThreadPractice(); //Creating new thread;
+        t.start();//Start the thread execution using start method.
     }
 }
