e4e5f611bbcd7032fbbd0bd831304e2be6ab3576
Mugundhan G <mugundhan10@gmail.com>
2024-11-19 05:50:42 +0530

    Added Pendings

--- src/OOPConceptsProblems/ClassesAndObjects/Main.java
+++ src/OOPConceptsProblems/ClassesAndObjects/Main.java
@@ -1,5 +1,11 @@
 package OOPConceptsProblems.ClassesAndObjects;
+import java.util.stream.IntStream;
+import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // Display the number of available processors (cores)
+        int cores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Available Cores: " + cores); // Output number of cores on the system
+    }
 }
